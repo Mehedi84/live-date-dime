49d475c2f2cd2f38f7688dbd378172869aa91b77
Mehedi84 <59836443+Mehedi84@users.noreply.github.com>
2022-11-13 16:44:47 +0600

    RealTime Date Time

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('load',nowdateTime)
+function nowdateTime(){
+    var date = new Date();    
+    var daynumber = date.getDate();
+    var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var month = month[date.getUTCMonth()];
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var days = days[date.getDay()];
+    var hour = date.getHours();
+    var hour = hour < 10 ? '0'+ hour : hour;
+    var minute = date.getMinutes();
+    var minute = minute < 10 ? '0'+ minute : minute;
+    var secend = date.getSeconds();
+    var secend = secend < 10 ? '0'+ secend : secend;
+    var ampm = hour >= 12 ? 'PM' : 'AM';
+
+document.getElementById("month").innerHTML= month;
+document.getElementById("dayName").innerHTML= days;
+document.getElementById("day").innerHTML= daynumber;
+document.getElementById("hour").innerHTML= hour;
+document.getElementById("minute").innerHTML= minute;
+document.getElementById("secend").innerHTML= secend;
+document.getElementById("ampm").innerHTML= ampm;
+setTimeout(nowdateTime,200);
+}
